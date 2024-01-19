e356245784b8809d417ccfe091ec887064fd457c
Suhyugyeong <suhyugyeongsuh@gamil.com>
2024-01-19 16:05:43 +0900

    0119_server쪽 app.js/DAO.js/Router.js/프론트쪽 app.css/index.css수정

--- project_server/product/productRouter.js
+++ project_server/product/productRouter.js
@@ -2,54 +2,56 @@ const express = require("express");
 const router = express.Router();
 const productDAO = require("./productDAO");
 
-router.get("/", function (req, res) {
-  res.send("products.ok");
-});
-
-router.get("/detail/:id", function (req, res) {
-  res.send("detail.ok");
-});
-
-router.get("/bidding", function (req, res) {
-  res.send("bidding.ok");
-});
-
 // router.get("/productList", function (req, res, next) {
-//   console.log("loading...");
-//   boardDAO.productList((resp) => {
+//   console.log("상품 메인 불러오기");
+//   productDAO.productList((resp) => {
 //     res.json(resp);
 //   });
 // });
+//화면메인 부분 -준영님
 
-// router.get("/detail/:id", function (req, res, next) {
-//   console.log("router...");
-//   const id = req.params.id;
-//   productDAO.board(id, (resp) => {
-//     res.json(resp);
-//   });
+router.get("/detail/:id", function (req, res, next) {
+  console.log("디테일 불러오기");
+  const id = req.params.id;
+  productDAO.detail({ product_id: id }, (resp) => {
+    //productDAO.detail 함수의 매개변수로는 객체를 받도록 정의되어 있으니까 객체 안에 담아야함
+    res.json(resp);
+  });
+}); //성공
+
+router.post("/bidding", (req, res, next) => {
+  console.log("낙찰페이지 불러오기");
+  const data = req.body;
+  productDAO.bidding(data, (resp) => {
+    res.json(resp);
+  });
+});
+
+router.get("/biddingTable", function (req, res, next) {
+  console.log("낙찰테이블 불러오기");
+  productDAO.biddingTable((resp) => {
+    res.json(resp);
+  });
+}); //성공
+
+router.post("/update", function (req, res, next) {
+  console.log("게시글 수정하기");
+  const data = req.body;
+  productDAO.update(data, (resp) => {
+    res.json(resp);
+  });
+});
+
+// router.get("/", function (req, res) {
+//   res.send("products.ok");
 // });
 
-// router.post("/bidding", (req, res, next) => {
-//   console.log("insert router...");
-//   const data = req.body;
-//   boardDAO.insert(data, (resp) => {
-//     res.json(resp);
-//   });
+// router.get("/detail/:id", function (req, res) {
+//   res.send("detail.ok");
 // });
 
-// router.get("/biddingTable", function (req, res, next) {
-//   console.log("boardList loading...");
-//   boardDAO.boardList((resp) => {
-//     res.json(resp);
-//   });
-// });
-
-// router.post("/update", function (req, res, next) {
-//   console.log("update router...");
-//   const data = req.body;
-//   boardDAO.update(data, (resp) => {
-//     res.json(resp);
-//   });
+// router.get("/bidding", function (req, res) {
+//   res.send("bidding.ok");
 // });
 
 module.exports = router;
