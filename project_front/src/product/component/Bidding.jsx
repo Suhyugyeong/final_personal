c9be4e79f46292605b2006cdc84d7c04c8a53c02
Suhyugyeong <suhyugyeongsuh@gamil.com>
2024-01-19 20:52:30 +0900

    0119

--- project_front/src/product/component/Bidding.jsx
+++ project_front/src/product/component/Bidding.jsx
@@ -41,37 +41,50 @@
     <section class="checkout_area padding_top">
       <div class="container">
         <div class="returning_customer">
-          <div class="check_title">
+          <div class="check_title" style="background-color: grez;">
                  <div class="col-lg-12">
-                    <h2>책 검색</h2>
+                    <h2 style="color: #e83e8c;">책 검색</h2>
                 <div class="input-group mb-3">
                   <input type="text" class="form-control" placeholder="제목, 저자, ISBN" aria-label="Recipient's username" aria-describedby="button-addon2">
                   <button class="btn btn-outline-secondary" type="button" id="button-addon2">검색</button>
                 </div>
                 </div>
                 <div class="col-lg-12">
-                  <h2>입찰가격</h2>
+                  <h2 style="color: #e83e8c;">입찰가격</h2>
                 <div class="input-group mb-3">
                   <input type="text" class="form-control" placeholder="입찰가" aria-label="Recipient's username" aria-describedby="basic-addon2">
                   <span class="input-group-text" id="basic-addon2">원(won)</span>
                 </div>
-                <h2>품질등급</h2>
+                <h2 style="color: #e83e8c;">품질등급</h2>
+       
                 <select class="country_select">
                   <option value="1">상</option>
                   <option value="2">중</option>
-                  <option value="4">하</option>
+                  <option value="3">하</option>
                 </select>
+                <div>
+                  
+                </div>
+                
                 <section class="confirmation_part padding_top">
-                  <div class="container">
+                  <!-- <div class="container"> -->
+                    
                     <div class="row">
+                      
                       <div class="col-lg-12">
+                        
                         <div class="confirmation_tittle">
+                          
                           <span>품질 등급 안내</span>
+                          
                         </div>
+                        
                       </div>
+                      
+                      <img href="img/test.jpg"/>
                       <div class="col-lg-4 col-lx-4">
                         <div class="single_confirmation_details">
-                          <h4>상</h4>
+                          <h4 style="color: #e83e8c;">상</h4>
                           <ul>
                             <li>
                               <p>낙장, 찢어짐, 오염</p>
@@ -93,7 +106,7 @@
                       </div>
                       <div class="col-lg-4 col-lx-4">
                         <div class="single_confirmation_details">
-                          <h4>중</h4>
+                          <h4 style="color: #e83e8c;">중</h4>
                           <ul>
                             <li>
                               <p>낙장, 찢어짐, 오염</p>
@@ -115,7 +128,7 @@
                       </div>
                       <div class="col-lg-4 col-lx-4">
                         <div class="single_confirmation_details">
-                          <h4>하</h4>
+                          <h4 style="color: #e83e8c;">하</h4>
                           <ul>
                             <li>
                               <p>낙장, 찢어짐, 오염</p>
@@ -139,14 +152,14 @@
             </div>
             </div>
             </section>
-                <div class="col-lg-12">
+                <!-- <div class="col-lg-12"> -->
                   <div class="order_box">
                     <div class="input-group mb-3">
                       <div class="productResult"> 
-                        <img/>
+                        <!-- <img/> -->
                       </div>
                     </div>
-                    <h2>사진첨부</h2>
+                    <h2 style="color: #e83e8c;">사진첨부</h2>
                     <!-- <input
                     type="file"
                     class="real-upload"
@@ -158,8 +171,8 @@
                   <div class="mb-3">
                     <label for="formFile" class="form-label"></label>
                     <input class="form-control" type="file" id="formFile">
+                    <h2 style="color: #e83e8c;">상세설명(선택사항)</h2>
                   </div>
-                  <h2>상세설명(선택사항)</h2>
                   <div class="form-floating">
                     <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
                     <label for="floatingTextarea2">(예시)낙장은 없으나 오염 있음</label>
@@ -167,37 +180,28 @@
                     </ul>
                     <ul class="list">
                       <li>
-                        <a href="#">000
-                          <span>000</span>
-                        </a>
+          
                       </li>
                     <div class="payment_item">
                       <div class="radion_btn">
-                        <input type="radio" id="f-option5" name="selector" />
-                        <label for="f-option5">Check payments</label>
-                        <div class="check"></div>
+             
                       </div>
-                      <p>
-                        Please send a check to Store Name, Store Street, Store Town,
-                        Store State / County, Store Postcode.
-                      </p>
+     
                     </div>
                     <div class="payment_item active">
                       <div class="radion_btn">
-                        <input type="radio" id="f-option6" name="selector" />
-                        <label for="f-option6">Paypal </label>
-                        <img src="img/product/single-product/card.jpg" alt="" />
-                        <div class="check"></div>
+                        <h2>입찰시 주의 사항</h2>
                       </div>
                       <p>
-                        Please send a check to Store Name, Store Street, Store Town,
-                        Store State / County, Store Postcode.
+                        입찰자는 해당 상품을 입찰하는 데 동의합니다.<br/>
+                        낙찰자가 결제하면 경매는 종료됩니다.<br/>
+                        낙찰자가 원치 않을 경우 경매는 유찰될 수 있습니다.<br/>
                       </p>
                     </div>
                     <div class="creat_account">
                       <input type="checkbox" id="f-option4" name="selector" />
-                      <label for="f-option4">최종 입찰에 동의합니다</label>
-                      <a href="#">terms & conditions*</a>
+                      <label for="f-option4" style="color: #e83e8c;">최종 입찰에 동의합니다</label>
+            
                     </div>
                     <!-- <a class="btn_3" href="#">Proceed to Paypal</a>
                     <a class="btn_3" href="#">Proceed to Paypal</a> -->
