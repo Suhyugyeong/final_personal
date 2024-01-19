95c6e9fa230666808c2add5d1c01abee5d25acf8
Suhyugyeong <suhyugyeongsuh@gamil.com>
2024-01-19 16:33:45 +0900

    0119

--- project_front/src/App.jsx
+++ project_front/src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import ProductMain from "./product/ProductMain";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      {/* header랑 footer 아직 없으니까 
+      /products/*로 해야할지? /product/*로 해야할지? */}
+      <Routes>
+        <Route path="/product/*" element={<ProductMain />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- project_front/src/product/ProductMain.jsx
+++ project_front/src/product/ProductMain.jsx
@@ -1,14 +1,19 @@
 import { Route, Routes } from "react-router-dom";
-import ProductList from "./component/ProductList";
 import Detail from "./component/Detail";
+import Bidding from "./component/Bidding";
+import Update from "./component/Update";
 
 const ProductMain = () => {
   return (
     <div>
-      <h2>제품 Main</h2>
+      <h2>제품 메인 페이지</h2>
       <Routes>
-        <Route path="/list" element={<ProductList />} />
         <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/bidding" element={<Bidding />} />
+        {/* biddingTable이 detail 페이지 내부에 만들어지는데... 이걸 새 component로 만들어야 하는지,
+        아니면 그냥 Detail 안쪽에 테이블을 넣으면 되는지, 
+      detail페이지의 수정은 Update로 따로 만들어야겠지?*/}
+        <Route path="/update/:id" element={<Update />} />
       </Routes>
     </div>
   );
