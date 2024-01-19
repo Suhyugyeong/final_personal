1025681e3a3f65a37b9fa7dbb61908ef1e47571a
Suhyugyeong <suhyugyeongsuh@gamil.com>
2024-01-19 17:19:21 +0900

    0119_템플릿수정중

---
+++ project_front/src/product/component/Detail.jsx
@@ -0,0 +1,225 @@
+return(
+    <div>
+        <!DOCTYPE html>
+<html lang="zxx">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <title>aranoz</title>
+    <link rel="icon" href="img/favicon.png" />
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <!-- animate CSS -->
+    <link rel="stylesheet" href="css/animate.css" />
+    <!-- owl carousel CSS -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css" />
+    <link rel="stylesheet" href="css/lightslider.min.css" />
+    <!-- font awesome CSS -->
+    <link rel="stylesheet" href="css/all.css" />
+    <!-- flaticon CSS -->
+    <link rel="stylesheet" href="css/flaticon.css" />
+    <link rel="stylesheet" href="css/themify-icons.css" />
+    <!-- font awesome CSS -->
+    <link rel="stylesheet" href="css/magnific-popup.css" />
+    <!-- style CSS -->
+    <link rel="stylesheet" href="css/style.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </head>
+
+  <body>
+    <!--================Single Product Area =================-->
+    <div className="product_image_area section_padding">
+      <div className="container">
+        <div className="row s_product_inner justify-content-between">
+          <div className="col-lg-7 col-xl-7">
+            <div className="product_slider_img">
+              <div id="vertical">
+                <div data-thumb="img/product/single-product/book_img.jpg">
+                  <img src="img/product/single-product/book_img.jpg" />
+                </div>
+                <div data-thumb="img/product/single-product/product_1.png">
+                  <img src="img/product/single-product/product_1.png" />
+                </div>
+                <div data-thumb="img/product/single-product/product_1.png">
+                  <img src="img/product/single-product/product_1.png" />
+                </div>
+                <div data-thumb="img/product/single-product/product_1.png">
+                  <img src="img/product/single-product/product_1.png" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-5 col-xl-4">
+            <div className="s_product_text">
+              <h3>상대성이론</h3> <br/>
+              <ul className="list">
+                  <li>
+                    <a href="#"> <span>저자</span> : In Stock</a>
+                  </li>
+                  <li>
+                    <a href="#"> <span>ISBN</span> : In Stock</a>
+                  </li>
+                  <li>
+                    <a href="#"> <span>출판년도</span> : In Stock</a>
+                  </li>
+                  <br/>
+                </ul>
+                <p>
+                </p>
+                      <h3>입찰 종료 시간</h3><br/>
+                      <a href="#" className="btn_3">판매입찰하기</a>
+                      <h2>즉시구매가</h2>
+                     
+              </div>
+              
+            </div>
+            
+          </div>
+        </div>
+        
+      </div>
+      <div className="container">
+        <p>
+          Beryl Cook is one of Britain’s most talented and amusing artists
+          .Beryl’s pictures feature women of all shapes and sizes enjoying
+          themselves .Born between the two world wars, Beryl Cook eventually
+          left Kendrick School in Reading at the age of 15, where she went
+          to secretarial school and then into an insurance office. After
+          moving to London and then Hampton, she eventually married her next
+          door neighbour from Reading, John Cook. He was an officer in the
+          Merchant Navy and after he left the sea in 1956, they bought a pub
+          for a year before John took a job in Southern Rhodesia with a
+          motor company. Beryl bought their young son a box of watercolours,
+          and when showing him how to use it, she decided that she herself
+          quite enjoyed painting. John subsequently bought her a child’s
+          painting set for her birthday and it was with this that she
+          produced her first significant work, a half-length portrait of a
+          dark-skinned lady with a vacant expression and large drooping
+          breasts. It was aptly named ‘Hangover’ by Beryl’s husband and
+          <br/>
+          <br/>
+          <br/>
+        </p>
+    </div>
+    <!--================End Single Product Area =================-->
+    <section className="confirmation_part padding_top">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="confirmation_tittle">
+              <span>입찰 현황을 확인하세요.</span>
+            </div>
+          </div>
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">입찰자</th>
+                <th scope="col">입찰금액(원)</th>
+                <th scope="col">상품상태</th>
+                <!-- <th scope="col">문의하기</th> -->
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>30,000</td>
+                <td>중</td>
+                <!-- <td><button type="button" className="btn btn-secondary">문의</button></td> -->
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>38,000</td>
+                <td>상</td>
+                <!-- <td><button type="button" className="btn btn-secondary">문의</button></td> -->
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry the Bird</td>
+                <td>29,000</td>
+                <td>중</td>
+                <!-- <td><button type="button" className="btn btn-secondary">문의</button></td> -->
+              </tr>
+              <tr>
+                <th scope="row">4</th>
+                <td>Larry the Bird</td>
+                <td>29,000</td>
+                <td>중</td>
+                <!-- <td><button type="button" className="btn btn-secondary">문의</button></td> -->
+              </tr>
+              <tr>
+                <th scope="row">5</th>
+                <td>Larry the Bird</td>
+                <td>29,000</td>
+                <td>중</td>
+                <!-- <td><button type="button" className="btn btn-secondary">문의</button></td> -->
+              </tr>
+            </tbody>
+          </table>
+        <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+          <!-- <button className="btn btn-danger" type="button">삭제</button> -->
+          <button className="btn btn-warning" type="button">수정</button>
+        </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-lx-4">
+            </div>
+          </div>
+          <div className="col-lg-4 col-lx-4">
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-12">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--================ confirmation part end =================-->
+  
+    <!--================Product Description Area =================-->
+    <section className="product_description_area">
+    </section>
+
+    <!-- jquery plugins here-->
+    <!-- jquery -->
+    <script src="js/jquery-1.12.1.min.js"></script>
+    <!-- popper js -->
+    <script src="js/popper.min.js"></script>
+    <!-- bootstrap js -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- easing js -->
+    <script src="js/jquery.magnific-popup.js"></script>
+    <!-- swiper js -->
+    <script src="js/lightslider.min.js"></script>
+    <!-- swiper js -->
+    <script src="js/masonry.pkgd.js"></script>
+    <!-- particles js -->
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.nice-select.min.js"></script>
+    <!-- slick js -->
+    <script src="js/slick.min.js"></script>
+    <script src="js/swiper.jquery.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/contact.js"></script>
+    <script src="js/jquery.ajaxchimp.min.js"></script>
+    <script src="js/jquery.form.js"></script>
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/mail-script.js"></script>
+    <script src="js/stellar.js"></script>
+    <!-- custom js -->
+    <script src="js/theme.js"></script>
+    <script src="js/custom.js"></script>
+  </body>
+</html>
+</div>
+)
