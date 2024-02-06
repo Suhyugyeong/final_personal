a954950cfb2611d1f7e6c44384f45a30f66790dd
Suhyugyeong <suhyugyeongsuh@gamil.com>
2024-02-06 17:45:05 +0900

    0206 detail에 UserContext넣음, Update는 글 수정이라 후에 buy 페이지에 연결해야함

--- project_front/src/product/component/Update.jsx
+++ project_front/src/product/component/Update.jsx
@@ -1,75 +1,40 @@
 import axios from "axios";
-import React, { useCallback, useState, useEffect, useContext } from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import UserContext from "../../UserContext";
 
 const Update = () => {
   const navigate = useNavigate();
-  const context = useContext(UserContext);
-  const loggedInUserEmail = context.state.userData.email;
   const { product_id } = useParams();
+  //해당 url에서 product_id 받아올거고
   const [product, setProduct] = useState({
     master_price: "",
     content: "",
+    //여기는 바꿀 수 있는 부분만 넣는건가? 변경될 내용? 초기값...
   });
 
   const changeData = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
+    //[e.target.name] 변경된 input요소 이름
   };
-  //변경된 필드만 값 업데이트
 
   const getData = async () => {
-    try {
-      const resp = await axios.get(
-        "http://localhost:8000/products/detail/" + product_id
-      );
-      setProduct(resp.data.data);
-
-      // 권한 확인 - 작성자와 로그인한 사용자가 다를 경우 수정 권한이 없음
-      if (resp.data.data.email !== loggedInUserEmail) {
-        // 권한이 없으면 다른 페이지로 이동(컴포넌트 만들어야 되나?)
-        navigate("/unAuthorizedPage");
-      }
-    } catch (error) {
-      console.error("데이터 가져오기 실패", error);
-    }
+    const resp = await axios.get(
+      "http://localhost:8000/products/detail/" + product_id
+    );
+    setProduct(resp.data.data);
   };
 
   useEffect(() => {
     getData();
-  }, [loggedInUserEmail, product_id]);
+  }, []);
 
-  const detailUpdate = async () => {
-    try {
-      await axios.post("http://localhost:8000/products/update/" + product_id, {
-        product,
-      });
-      navigate("/product/detail/" + product_id);
-    } catch (error) {
-      console.error("업데이트 실패", error);
-    }
+  const detailUpdate = async (e) => {
+    await axios.post("http://localhost:8000/products/update/" + product_id, {
+      product,
+    });
+    //서버에서 json 형식으로 데이터를 받기를..
+    navigate("product/detail/" + product_id);
   };
-
-  return (
-    <div>
-      {/* 수정 페이지의 HTML 렌더링을 추가 */}
-      <h1>수정 페이지</h1>
-      {/* 예시로 수정 내용의 input을 추가 */}
-      <input
-        type="text"
-        name="content"
-        value={product.content}
-        onChange={changeData}
-      />
-      {/* 수정 버튼 클릭 시 detailUpdate 함수 호출 */}
-      {loggedInUserEmail ? <button onClick={detailUpdate}>수정</button> : ""}
-    </div>
-  );
+  return <h1>html 작성해야함</h1>;
 };
 export default Update;
-
-// {loggedInUserEmail === product.email ? (
-//   <button className="btn btn-warning" type="button">
-//   수정
-//   </button>
-// ) : ''} 은영님 피드백
